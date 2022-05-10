7e8cc8b2a93f0cec52608a2e26267f79116dc903
toantran201 <toantran201.w@gmail.com>
2022-05-10 23:46:51 +0700

    Inint

---
+++ src/initial-data.js
@@ -0,0 +1,12 @@
+const initialData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'Take out the garbage'},
+        'task-2': {id: 'task-2', content: 'Take out the garbage'},
+        'task-3': {id: 'task-3', content: 'Take out the garbage'},
+        'task-4': {id: 'task-4', content: 'Take out the garbage'},
+        'task-5': {id: 'task-5', content: 'Take out the garbage'},
+    },
+    columns: {
+
+    }
+}
